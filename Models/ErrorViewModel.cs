78551f26d7d38778ffd0c537187de001a568e951
gladmoresangare <gladmoresangare@gmail.com>
2024-09-30 22:35:03 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrestaHospitality.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
